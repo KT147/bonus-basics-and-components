a23bac6e6a68a9592188bca3254629839462a3ed
KT147 <kevin.tammm@gmail.com>
2025-10-08 12:09:19 +0300

    min ja max väärtused inputtidel

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,20 +1,21 @@
-import Card from '../UI/Card';
-import ExpenseDate from './ExpenseDate';
-import './ExpenseItem.css'
+import Card from "../UI/Card";
+import ExpenseDate from "./ExpenseDate";
+import "./ExpenseItem.css";
 
-function ExpenseItem({expenses}) {
+function ExpenseItem({ expenses }) {
   return (
     <>
-    {expenses.map(expense =>
-    <Card key={expense.id} className="expense-item">
-      <div>
-        <ExpenseDate expense={expense} />
-      </div>
-      <div className='expense-item__description'>
-        <h2>{expense.title}</h2>
-        <div className='expense-item__price'>${expense.amount}</div>
-      </div>
-    </Card>)}
+      {expenses.map((expense) => (
+        <Card key={expense.id} className="expense-item">
+          <div>
+            <ExpenseDate expense={expense} />
+          </div>
+          <div className="expense-item__description">
+            <h2>{expense.title}</h2>
+            <div className="expense-item__price">${expense.amount}</div>
+          </div>
+        </Card>
+      ))}
     </>
   );
 }
